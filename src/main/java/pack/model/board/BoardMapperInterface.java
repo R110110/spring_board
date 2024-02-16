1f2f0db621c9562b4d97c12d37890d8a51eee658
R110110 <126158375+R110110@users.noreply.github.com>
2024-02-16 21:13:05 +0900

    설계에 따른 패키지, 자바 파일 틀 구성, 마이바티스 맵핑 인터페이스 작성

--- src/main/java/pack/model/board/BoardMapperInterface.java
+++ src/main/java/pack/model/board/BoardMapperInterface.java
@@ -1,8 +1,32 @@
 package pack.model.board;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface BoardMapperInterface {
-
+	
+	@Select("select * from board order by post_date limit 10")
+	List<BoardDto> selectBoardHead10();
+	
+	@Select("select * from board where post_no = #{post_no}")
+	List<BoardDto> selectPostDetail(int post_no);
+	
+	@Select("")
+	List<BoardDto> selectSearchedPost(String word);
+	
+	@Insert("insert into board (post_name, post_body, user_no) values(#{post_name}, #{post_body}, #{user_no})")
+	int posting(String post_name, String post_body, int user_no);
+	
+	@Update("update board set post_name = #{post_name} and post_body = #{post_body} "
+			+ "where post_no = #{post_no} and post_user_no = #{post_user_no}")
+	int updatePost(String post_name, String post_body, int post_no, int post_user_no);
+	
+	@Delete("delete from board where post_no = #{post_no} and post_user_no = #{post_user_no}")
+	int deletePost(int post_no, int post_user_no);
 }
--- src/main/java/pack/model/user/UserMapperInterface.java
+++ src/main/java/pack/model/user/UserMapperInterface.java
@@ -1,8 +1,13 @@
 package pack.model.user;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapperInterface {
-
+	
+	@Select("select * from user where user_no = #{user_no}")
+	List<UserDto> selectUserInfo(int user_no);
 }
