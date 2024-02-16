7f17e2d90b6a361be99baa97aa875fa3a0bb99fc
R110110 <126158375+R110110@users.noreply.github.com>
2024-02-17 04:19:13 +0900

    Dto 작성

--- src/main/java/pack/model/board/BoardDto.java
+++ src/main/java/pack/model/board/BoardDto.java
@@ -1,8 +1,12 @@
 package pack.model.board;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
 public class BoardDto {
-
+	private int post_no, post_user_no;
+	private String post_name, post_body;
+	private LocalDate post_date;
 }
--- src/main/java/pack/model/user/UserDto.java
+++ src/main/java/pack/model/user/UserDto.java
@@ -1,8 +1,13 @@
 package pack.model.user;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
 public class UserDto {
-
+	private int user_no;
+	private String user_id, user_pwd, user_mail;
+	private LocalDate user_join_date;
+	private boolean user_enable;
 }
